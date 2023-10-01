6dead098d4db016c0cc57b5406981595e7580a21
ferdouszihad <f.zihad333@gmail.com>
2023-10-01 16:18:40 +0600

    reQuiremnet Anlaysed

---
+++ src/Routers/mainRoutes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const mainRoutes = createBrowserRouter([
+  {
+    path: "/",
+    element: <h2>This is Main</h2>,
+  },
+]);
+
+export default mainRoutes;
